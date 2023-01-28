ba7bac9abfb7b4936f0389b293a42d2b77aa1f64
submarines-and <johannes.sorensen@ingka.ikea.com>
2023-01-28 18:14:49 +0100

    Working

--- Shared (Extension)/Resources/content.js
+++ Shared (Extension)/Resources/content.js
@@ -1,7 +1,39 @@
-browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
-    console.log("Received response: ", response);
-});
+function debounce(func, wait) {
+    wait = wait ?? 50;
+    
+    let timeout;
+    
+    return () => {
+        if (timeout) {
+            clearTimeout(timeout);
+        }
+        timeout = setTimeout(func, wait)
+    }
+}
 
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
-});
+function elementIsVisible(element) {
+  if (!element) {
+    return false;
+  }
+
+  const bounds = element.getBoundingClientRect();
+  const viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
+  return !(bounds.bottom < 0 || bounds.top - viewHeight >= 0);
+}
+
+
+
+document.addEventListener("scroll", debounce(() => {
+    const moreButton = document.querySelector(".catalog-bottom-container a");
+    
+    let isLoading = false;
+    if(elementIsVisible(moreButton) && !isLoading){
+        isLoading = true;
+        moreButton.click();
+        
+        // throttling
+        setTimeout(() => {
+            isLoading = false;
+        }, 100);
+    }
+}))
