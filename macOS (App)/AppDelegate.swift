e0ca632617c22e04132f3e58f0b9f4714971cb93
submarines-and <johannes.sorensen@ingka.ikea.com>
2023-01-28 17:19:20 +0100

    Initial Commit

---
+++ macOS (App)/AppDelegate.swift
@@ -0,0 +1,16 @@
+// AppDelegate.swift
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
